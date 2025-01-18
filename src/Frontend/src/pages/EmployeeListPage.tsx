f045de2578dc0395763fed11a6468ab166b688a1
Massimo Altamura <158032882+MassimoAltamura@users.noreply.github.com>
2025-01-18 11:11:17 +0000

    impostatura frontend della tabella di EmployeeListPage

--- src/Frontend/src/pages/EmployeeListPage.tsx
+++ src/Frontend/src/pages/EmployeeListPage.tsx
@@ -1,5 +1,65 @@
+import { Paper, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import  { useEffect, useState } from "react";
+
+interface EmployeeListQueryResponse {
+    Id: number;
+    Code:number;
+    FirstName:string;
+    LastName:string;
+    Address:string;
+    Email:string;
+    Phone:string;
+}
+
 export default function EmployeeListPage() {
-    return <>
-        <i>Todo employees</i>
+    const [lista, setLista]=useState<EmployeeListQueryResponse[]>([])
+    useEffect(()=>{
+        fetch("api/employees/list")
+        .then((response)=>{
+            return response.json()
+        })
+        .then((data)=>{
+            setLista(data as EmployeeListQueryResponse[])
+        })
+    })
+   
+    return<>
+    <Typography variant="h4" sx={{textAlign:"center", mt:4, mb:4}}>
+        Employee
+    </Typography>
+    
+    <TableContainer component={Paper}>
+        <Table sx={{minWidth: 650}} aria-label="simple table">
+            <TableHead>
+                <TableRow>
+                    <StyledTableHeadCell>FirstName</StyledTableHeadCell>
+                    <StyledTableHeadCell>LastName</StyledTableHeadCell>
+                    <StyledTableHeadCell>Adress</StyledTableHeadCell>
+                    <StyledTableHeadCell>Email</StyledTableHeadCell>
+                    <StyledTableHeadCell>Phone</StyledTableHeadCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {lista.map((row)=>(
+                    <TableRow
+                    key={row.Id}
+                    sx={{"&:last-child td, &:last-child th": { border: 0 }}}
+                    >
+                        <TableCell>{row.FirstName}</TableCell>
+                        <TableCell>{row.LastName}</TableCell>
+                        <TableCell>{row.Address}</TableCell>
+                        <TableCell>{row.Email}</TableCell>
+                        <TableCell>{row.Phone}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    </TableContainer>
     </>
 }
+const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.primary.light,
+      color: theme.palette.common.white,
+    },
+  }));
